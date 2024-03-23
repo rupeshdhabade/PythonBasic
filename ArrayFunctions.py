7a56964eb79af898978034dc29c672b0379a6c65
Rupesh Dhabade <rupesh.dhabade@gmail.com>
2024-03-23 10:08:09 +0530

    FirstCommit

---
+++ ArrayFunctions.py
@@ -0,0 +1,45 @@
+data=[100,250,10,40,50]
+# data.append(100)
+# data.append(99)
+# print(data)
+
+# data.pop(2) #It removes the value from given position
+# print(data)
+
+# data.clear()# It make entire array blank
+# print(data)
+
+print(data)
+sd=data.copy()
+print(sd)
+
+
+
+# i=data.index(20)
+# print("Index of 20= " + str(i))
+
+
+# i=data.index(50)
+# print("Index of 50= " + str(i))
+
+# print(data)
+# data.insert(3,500)
+# print(data)
+
+
+# print(data)
+# data.remove(40)
+# print(data)
+
+# print(data)
+# data.reverse()
+# print(data)
+
+# print(data)
+# data.sort()
+# print(data)
+# data.sort(reverse=True)
+# print(data)
+
+
+
